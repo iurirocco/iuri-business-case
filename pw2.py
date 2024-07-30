f35c9a360ccc201a602b19da33c89441a7bf44d8
Iuri Rocco <irocco@indeed.com>
2024-07-29 21:34:07 -0300

    Adjusting verbiage

--- pw2.py
+++ pw2.py
@@ -3,7 +3,7 @@ import bcrypt
 # script to prompt for username, password, confirm the password, and store the hashed password
 
 # Get username from user
-username = input("Enter your username: ")
+username = input("Enter the desired username: ")
 
 # Get password from user
 password = input("Enter your password: ")
